5a91157bd67ec3012cb83519c87847a9e57967c6
M. Brock Podgurski <mbpod10@gmail.com>
2022-04-22 14:10:09 -0400

    create header and cart

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import meals from '../../assets/meals.jpeg'
+import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton'
+
+const Header = () => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton />
+      </header>
+
+      <div className={classes['main-image']}>
+        <img src={meals} alt="food" />
+      </div>
+    </>
+  )
+}
+
+export default Header
